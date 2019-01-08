d9d8ac838a260bf6b706196fc36514d6d276890d
Diep Truong <29584874+dtruong11@users.noreply.github.com>
2019-01-07 21:29:41 -0800

    Created progress bar & fixed broken images

--- src/components/cards/Card.js
+++ src/components/cards/Card.js
@@ -1,35 +1,75 @@
-import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardFooter, CardTitle, Row, Col } from 'reactstrap'
+import React, { Component } from 'react';
+import { Card, CardBody, CardText, CardFooter, CardTitle, Row, Col } from 'reactstrap'
+import { Icon } from 'react-icons-kit'
+import { users } from 'react-icons-kit/feather/users'
+import { eye } from 'react-icons-kit/feather/eye'
+import { ecommerce_money } from 'react-icons-kit/linea/ecommerce_money'
 import '../../styles/cards.css'
 
-const SingleCard = ({ title, views, subscribers, price, pic, currency }) => {
+const ProgressBar = ({ percentage }) => {
   return (
-    <div>
-      <Card className='event_card'>
-        <div>
-          <div className='card_img'
-            style={{
-              background: `url('${pic}')`,
-              height: '200px',
-              backgroundSize: 'cover',
-              backgroundPosition: 'center'
-            }}></div>
+    <div className="progress_bar">
+      <div className="filler" style={{ width: `${percentage}%` }} />      
+    </div>
+  )
+}
+
+class SingleCard extends Component {  
+  constructor(props) {
+    super (props)
+    this.state = {
+      percentage: 100
+    }
+  }
+  
+  render(){
+  let { title, views, subscribers, price, pic, currency } = this.props 
+  return (
+    <div className='event_card'>
+      <Card>
+        <div className='card_img'
+          style={{
+            background: `url('${pic}'), url('https://source.unsplash.com/random')`,
+            height: '150px',
+            backgroundSize: 'cover',
+            backgroundPosition: 'center'
+          }}>
         </div>
-        {/* <CardImg top width="100%" src={pic} alt="Card image cap" /> */}
         <CardBody>
           <CardTitle>{title}</CardTitle>
+          <div>
+            <div className='status'>
+              <div className='status_child'>$ 5000/ Month</div>
+              <div className='status_child'>Live </div>
+            </div>
+            <ProgressBar percentage={this.state.percentage}/>
+          </div>
         </CardBody>
         <CardFooter>
-          <Row>
-            <Col>{currency} {price}</Col>
-            <Col>{subscribers}</Col>
-            <Col>{views}</Col>
+          <Row className='card_info'>
+            <Col xs={5}>
+              <CardText>
+              <Icon size={15} icon={ecommerce_money}/>
+              {currency} {price}
+              </CardText></Col>
+            <Col>
+              <Row>
+                <Col>
+                  <Icon size={15} icon={users} />
+                  <CardText>{subscribers}</CardText>
+                </Col>
+                <Col>
+                  <Icon size={15} icon={eye} />
+                  <CardText>{views}</CardText>
+                </Col>
+              </Row>
+            </Col>
           </Row>
         </CardFooter>
       </Card>
     </div>
   )
-
+}
 }
 
 export default SingleCard  
