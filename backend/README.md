e4a182208949ba2d323c165db50218ac3f286074
Diep Truong <29584874+dtruong11@users.noreply.github.com>
2019-01-10 16:56:21 -0800

    added readme

---
+++ backend/README.md
@@ -0,0 +1,9 @@
+## UI Challenge _ Backend 
+
+#### Routes 
+* GET /cards -> return cards.json 
+* GET /campaigns -> return campaigns.json 
+
+#### Set up 
+1. npm install 
+2. npm run dev 
--- backend/src/controllers/controllers.js
+++ backend/src/controllers/controllers.js
@@ -1,13 +1,21 @@
 const model = require('../model/model')
 
 const getCards = (req, res, next) => {
-  const cards = model.getCards()
-  res.send({ cards })
+  try {
+    const cards = model.getCards()
+    res.send({ cards })
+  } catch (e) {
+    console.error(e)
+  }
 }
 
 const getCampaigns = (req, res, next) => {
-  const campaigns = model.getCampaigns()
-  res.send({ campaigns })
+  try {
+    const campaigns = model.getCampaigns()
+    res.send({ campaigns })
+  } catch (e) {
+    console.error(e)
+  }
 }
 
 module.exports = {
